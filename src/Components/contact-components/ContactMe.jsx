f72fb3d289fbf57593097c693987e9215f4be2c8
chichi5454 <140154706+chichi5454@users.noreply.github.com>
2024-02-02 13:20:38 +0300

    added functionality for contact form

--- src/Components/contact-components/ContactMe.jsx
+++ src/Components/contact-components/ContactMe.jsx
@@ -1,24 +1,56 @@
 /** @format */
+
 import "./contactme.css";
-import React from "react";
+import { useRef, useEffect, useState } from "react";
+import emailjs from "@emailjs/browser";
 
 const ContactMe = () => {
+  const emailRef = useRef();
+  const nameRef = useRef();
+  const messageRef = useRef();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => emailjs.init("2zeK6PBfb_aTdRrdY"), []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const serviceId = "service_36z5err";
+    const templateId = "lora@2024";
+    try {
+      setLoading(true);
+      await emailjs.send(serviceId, templateId, {
+        name: nameRef.current.value,
+        recipient: emailRef.current.value,
+        message: messageRef.current.value,
+      });
+      alert("Email successfully sent.");
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="contact-page">
+    <div className="contact-me">
       <h2>CONTACT ME</h2>
-      <div className="contact-page-content">
-        <h5>For Business Inquiries, Contact Me..... {""}</h5>
-        <form>
-          <input type="text" placeholder="Enter Name" />
-          <input
-            type="email"
-            id="email"
-            name="email"
-            placeholder="Enter your Email"
-          />
-          <input type="text" placeholder="Enter Message" />
-          <button>
-            <a href="mailto:chichi54lora@.com"> SEND</a>
+      <div className="form-page">
+        <p>For Business Inquiries, Contact Me.....</p>
+        <form className="for" onSubmit={handleSubmit}>
+          <div className="form_group">
+            <label htmlFor="">name</label>
+            <input ref={nameRef} placeholder="enter your name" />
+          </div>
+          <div className="form_group">
+            <label htmlFor="">email</label>
+            <input ref={emailRef} type="email" placeholder="enter your email" />
+          </div>
+          <div className="form_group">
+            <label htmlFor="">Message</label>
+            <input ref={messageRef} placeholder="enter your message" />
+          </div>
+          <button className="btn" disabled={loading}>
+            SEND
           </button>
         </form>
       </div>
